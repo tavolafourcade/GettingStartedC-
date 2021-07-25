066c88197fccc2644f61390e79636186dc2b9db2
tavolafourcade <tavolafourcade@gmail.com>
2021-07-24 19:14:42 -0500

    Tried more examples

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace Application1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Where should we go in May?");
+            WeatherUtilities.Report("San Francisco", WeatherUtilities.FahrenheitToCelsius(65), 73);
+            WeatherUtilities.Report("Denver", WeatherUtilities.FahrenheitToCelsius(77), 55);
             WeatherUtilities.Report("Bologna", 23, 65); 
         }
     }
